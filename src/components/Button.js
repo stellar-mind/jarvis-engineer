6b22736c28666831a0764b082ffd580e474ce81e
= <=>
2025-03-22 22:20:48 -0300

    Adiciona componente de botão funcional

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,24 +2,22 @@
 import React from 'react';
 import { TouchableOpacity, Text, StyleSheet } from 'react-native';
 
-const Button = ({ text, color, onPress }) => {
-  return (
-    <TouchableOpacity onPress={onPress} style={[styles.button, {backgroundColor: color}]}>
-      <Text style={styles.text}>{text}</Text>
-    </TouchableOpacity>
-  );
-};
+const Button = ({ text, color, onPress }) => (
+  <TouchableOpacity onPress={onPress} style={[styles.button, { backgroundColor: color }]}>
+    <Text style={styles.text}>{text}</Text>
+  </TouchableOpacity>
+);
 
 const styles = StyleSheet.create({
   button: {
-    padding: 10,
-    borderRadius: 5,
     justifyContent: 'center',
     alignItems: 'center',
+    padding: 10,
+    borderRadius: 5,
   },
   text: {
-    color: 'white',
-    textAlign: 'center',
+    color: '#fff',
+    fontSize: 16,
   },
 });
 
