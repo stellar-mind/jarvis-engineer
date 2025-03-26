c1901951828e43fe82c6982524eb122aeae67671
= <=>
2025-03-26 12:57:48 -0300

    Adiciona componente de botão funcional com props personalizadas

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,25 +1,23 @@
 import React from 'react';
 import { TouchableOpacity, Text, StyleSheet } from 'react-native';
 
-const Button = ({ text, color, onPress }) => {
-  return (
-    <TouchableOpacity onPress={onPress} style={[styles.button, {backgroundColor: color}]}>
-      <Text style={styles.text}>{text}</Text>
-    </TouchableOpacity>
-  );
-};
+const Button = ({ text, color, onPress }) => (
+  <TouchableOpacity style={[styles.button, { backgroundColor: color }]} onPress={onPress}>
+    <Text style={styles.text}>{text}</Text>
+  </TouchableOpacity>
+);
 
 const styles = StyleSheet.create({
   button: {
+    justifyContent: 'center',
+    alignItems: 'center',
     padding: 10,
     borderRadius: 5,
-    justifyContent: 'center',
-    alignItems: 'center'
   },
   text: {
     color: '#fff',
-    textAlign: 'center'
-  }
+    textAlign: 'center',
+  },
 });
 
 export default Button;
